066bed2cfe8f44a6ea8b590091d3145ff5330d7d
rickarof <kryonz_ricardo@hotmail.com>
2016-08-29 22:51:26 -0300

    teste
    teste

--- view/Marker/Listar.php
+++ view/Marker/Listar.php
@@ -1,32 +1,46 @@
 <section>
-<?php 
-	foreach ($markers as $marker) {
-		$a[] = $marker->nome;
+<?php
+foreach($markers as $marker){
+	 echo $marker->nome;
+	 echo $marker->lat;
+	 		echo '<br>';
+	for ($i=0;$i<6;$i++){
+	 	for ($o=0; $o<6;$o++){
+	 		echo '<script>';
+	 		echo 'var markers['.$i.']['.$o.']= $marker->id';
+	 		echo 'var markers['.$i.']['.$o.']= $marker->nome';
+	 		echo 'var markers['.$i.']['.$o.']= $marker->lat';
+	 		echo 'var markers['.$i.']['.$o.']= $marker->longi';
+	 		echo 'var markers['.$i.']['.$o.']= $marker->categoria';
+	 		echo 'var markers['.$i.']['.$o.']= $marker->descricao';
+	 		echo '</script>';
+		}
 	}
-	echo $a[1];
+}
 ?>
-
- 
-
-				<div id="map" style="height:500px; width:100%; position:relative;">
-				<script type="text/javascript">
-				alert(a[1]);
+<div id="map" style="height:500px; width:100%; position:relative;">
+<script type="text/javascript">
+alert(markers[1][2]);
 var map;
 function initMap() {
+
 var garopaba = {lat: -28.024537, lng: -48.623806};
+
   map = new google.maps.Map(document.getElementById('map'), {
     center: garopaba,
     zoom: 13
  });
 
- 
-
-	markers.nome = new google.maps.Marker({
+	markersnome = new google.maps.Marker({
     position: {lat: -28.024537, lng: -48.623806},
     map: map
-	
+});
+    markersilveira = new google.maps.Marker({
+    	position: {lat:markers[1][2], lng:markers[1][3]};
+    	map:map
   });
 }
+
  </script>
 
 					
