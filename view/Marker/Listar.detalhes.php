07ea8d41bbf8a14418e6c0388e70bde6a5a62010
Eduardo <eduardofidera@gmail.com>
2016-10-21 21:32:15 -0200

    Revert "Revert "método listar aprimorado, revisao geral, imagens""
    This reverts commit ecb724473fb36c6f36e684d7d8e8d1dc3412893d.

---
+++ view/Marker/Listar.detalhes.php
@@ -0,0 +1,42 @@
+<?php
+				echo "list = document.createElement('div');";
+				echo "list.setAttribute('class', 'list');";
+				
+				// Título e descricao
+				echo "detail = document.createElement('div');";
+				echo "detail.setAttribute('class', 'detail');";
+				echo "title = document.createElement('a');";
+				echo "title.setAttribute('class', 'title');";
+				echo "title.setAttribute('href', 'index.php?c=Marker&p=listarDetalhes&idmarker=$marker->id');";
+				echo "title.setAttribute('target', '_blank');";
+				
+				echo "t = document.createTextNode('$marker->nome');";
+				echo "title.appendChild(t);";
+				
+				
+				echo "descricao = document.createElement('p');";
+				echo "descricao.setAttribute('class', 'descricao');";
+				echo "t = document.createTextNode('$marker->descricao');";
+				echo "descricao.appendChild(t);";
+				echo "detail.appendChild(title);";
+				echo "detail.appendChild(descricao);";
+				
+
+				echo "list.appendChild(detail);";
+				
+				foreach ($imagens as $imagem) {
+					if ($imagem->idMarker == $marker->id) {
+							// Imagem
+							echo "photo = document.createElement('div');";
+							echo "createImg = document.createElement('img');";
+							echo "createImg.setAttribute('src', '$imagem->local');";
+							echo "createImg.setAttribute('class', 'imagem');";
+							echo "photo.appendChild(createImg);";
+							echo "list.appendChild(photo);";
+						
+					}
+				}
+				
+				
+				echo "document.getElementById('detalhes').appendChild(list);";
+?>
